6fa5b7db3929595302c43035681ef4d83d8e7202
bakboka <baa@skysize.io>
2025-09-27 17:40:23 +0200

    test nested

---
+++ test/skysize/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "PaaS",
+    "summary": """PaaS""",
+    "category": "",
+    "version": "18.0.1.0.1",
+    "author": "skysize.io",
+    "website": "https://www.skysize.io",
+    "license": "LGPL-3",
+    "depends": [
+        'base',
+    ],
+    "data": [
+
+    ],
+    'assets': {
+    },
+}
